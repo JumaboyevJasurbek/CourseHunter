3c38b10fe0b248618a494a39497ff83a08c0d201
abbosxon <abbosxon.oripov@yandex.ru>
2023-03-25 14:17:45 +0500

    change apiheader

--- src/module/users/users.controller.ts
+++ src/module/users/users.controller.ts
@@ -41,7 +41,7 @@ export class UsersController {
   }
 
   @ApiHeader({
-    name: "authorization",
+    name: "autharization",
     description: "User token",
     required: true
   })
@@ -54,7 +54,7 @@ export class UsersController {
 
 
   @ApiHeader({
-    name: "authorization",
+    name: "autharization",
     description: "User token",
     required: true
   })
